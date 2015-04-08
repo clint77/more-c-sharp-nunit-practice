bd843bcfa8cc8778adfabb16e624c849ca5f4a52
Clint <clintp1@gmail.com>
2015-04-08 11:06:01 +0100

    initial commit

---
+++ BookParsingC#v2/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BookParsingCv2
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			
+		}
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# More C-Sharp and NUnit Practice
